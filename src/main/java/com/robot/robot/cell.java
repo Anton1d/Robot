8171a8de7c3983d8746db45d932e9282238bf6e2
gigi11111 <redede1@gmail.com>
2022-06-01 13:55:55 +0200

    test de commit

--- src/main/java/com/robot/robot/cell.java
+++ src/main/java/com/robot/robot/cell.java
@@ -4,4 +4,6 @@ public class cell extends Map {
     static int coordonnéex;
     static int coordonnéey;
     public static int mur;
+    protected boolean murnord;
+    protected boolean murest;
 }
